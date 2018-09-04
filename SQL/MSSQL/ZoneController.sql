4483e567c47159d41f694e4649b5ac747139d377
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-09-04 15:59:40 +0200

    Changes to tables and new procedures
    Removed deprecated objects.
    Changed datatypes.
    Changed VARs in procedures.
    Created new procedures for some of the tables. Still missing a few, but the most important ones are done.

--- SQL/MSSQL/ZoneController.sql
+++ SQL/MSSQL/ZoneController.sql
@@ -18,6 +18,15 @@ IF OBJECT_ID('Auditing.ErrorHandling', 'P')	IS NOT NULL DROP PROCEDURE Auditing.
 -------------------------------------------------------------------------
 --Drop Procedures.
 -------------------------------------------------------------------------
+IF OBJECT_ID('OrderDetails.GetCourses', 'P')IS NOT NULL DROP PROCEDURE OrderDetails.GetCourses;
+IF OBJECT_ID('Person.GetUsers', 'P')		IS NOT NULL DROP PROCEDURE Person.GetUsers;
+IF OBJECT_ID('Person.UpdateUser', 'P')		IS NOT NULL DROP PROCEDURE Person.UpdateUser;
+IF OBJECT_ID('Person.RemoveUser', 'P')		IS NOT NULL DROP PROCEDURE Person.RemoveUser;
+IF OBJECT_ID('Person.AddUser', 'P')			IS NOT NULL DROP PROCEDURE Person.AddUser;
+IF OBJECT_ID('Config.GetRooms', 'P')		IS NOT NULL DROP PROCEDURE Config.GetRooms;
+IF OBJECT_ID('Config.UpdateRoom', 'P')		IS NOT NULL DROP PROCEDURE Config.UpdateRoom;
+IF OBJECT_ID('Config.RemoveRoom', 'P')		IS NOT NULL DROP PROCEDURE Config.RemoveRoom;
+IF OBJECT_ID('Config.AddRoom', 'P')			IS NOT NULL DROP PROCEDURE Config.AddRoom;
 IF OBJECT_ID('Config.GetVLANs', 'P')		IS NOT NULL DROP PROCEDURE Config.GetVLANs;
 IF OBJECT_ID('Config.UpdateVLAN', 'P')		IS NOT NULL DROP PROCEDURE Config.UpdateVLAN;
 IF OBJECT_ID('Config.RemoveVLAN', 'P')		IS NOT NULL DROP PROCEDURE Config.RemoveVLAN;
@@ -26,11 +35,9 @@ IF OBJECT_ID('Config.GetZones', 'P')		IS NOT NULL DROP PROCEDURE Config.GetZones
 IF OBJECT_ID('Config.UpdateZone', 'P')		IS NOT NULL DROP PROCEDURE Config.UpdateZone;
 IF OBJECT_ID('Config.RemoveZone', 'P')		IS NOT NULL DROP PROCEDURE Config.RemoveZone;
 IF OBJECT_ID('Config.AddZone', 'P')			IS NOT NULL DROP PROCEDURE Config.AddZone;
-IF OBJECT_ID('OrderDetails.GetCourses', 'P')IS NOT NULL DROP PROCEDURE OrderDetails.GetCourses;
 IF OBJECT_ID('Assets.GetSwitches', 'P')		IS NOT NULL DROP PROCEDURE Assets.GetSwitches;
 IF OBJECT_ID('Assets.UpdateSwitch', 'P')	IS NOT NULL DROP PROCEDURE Assets.UpdateSwitch;
 IF OBJECT_ID('Assets.RemoveSwitch', 'P')	IS NOT NULL DROP PROCEDURE Assets.RemoveSwitch;
-IF OBJECT_ID('Assets.AddSwitch_JSON', 'P')	IS NOT NULL DROP PROCEDURE Assets.AddSwitch_JSON;	--Deprecated
 IF OBJECT_ID('Assets.AddSwitch', 'P')		IS NOT NULL DROP PROCEDURE Assets.AddSwitch;
 IF OBJECT_ID('Assets.GetModels', 'P')		IS NOT NULL DROP PROCEDURE Assets.GetModels;
 IF OBJECT_ID('Assets.UpdateModel', 'P')		IS NOT NULL DROP PROCEDURE Assets.UpdateModel;
@@ -45,17 +52,10 @@ IF OBJECT_ID('OrderDetails.Orders', 'U')	IS NOT NULL DROP TABLE OrderDetails.Ord
 IF OBJECT_ID('OrderDetails.Courses', 'U')	IS NOT NULL DROP TABLE OrderDetails.Courses;
 IF OBJECT_ID('Person.Users', 'U')			IS NOT NULL DROP TABLE Person.Users;
 IF OBJECT_ID('Config.Rooms', 'U')			IS NOT NULL DROP TABLE Config.Rooms;
-IF OBJECT_ID('Config.PortConfig', 'U')		IS NOT NULL DROP TABLE Config.PortConfig;			--Deprecated
-IF OBJECT_ID('Config.ConfigJSON', 'U')		IS NOT NULL DROP TABLE Config.ConfigJSON;			--Deprecated
 IF OBJECT_ID('Config.VLANs', 'U')			IS NOT NULL DROP TABLE Config.VLANs;
 IF OBJECT_ID('Config.Zones', 'U')			IS NOT NULL DROP TABLE Config.Zones;
-IF OBJECT_ID('Assets.Switches_JSON', 'U')	IS NOT NULL DROP TABLE Assets.Switches_JSON;		--Deprecated
 IF OBJECT_ID('Assets.Switches', 'U')		IS NOT NULL DROP TABLE Assets.Switches;
 IF OBJECT_ID('Assets.Models', 'U')			IS NOT NULL DROP TABLE Assets.Models;
--------------------------------------------------------------------------
---Drop Sequence
--------------------------------------------------------------------------
-IF OBJECT_ID('Auditing.IDCounter', 'SO')	IS NOT NULL DROP SEQUENCE Auditing.IDCounter;		--Deprecated
 -------------------------------------------------------------------------
 --Drop Schema if it exist.
 -------------------------------------------------------------------------
@@ -82,14 +82,6 @@ GO
 CREATE SCHEMA Auditing;
 GO
 
---Currently deprecated, not used at the moment.
---Sequence for counting IDs
---CREATE SEQUENCE Auditing.IDCounter
---AS INT
---START WITH 0
---INCREMENT BY 1;
---GO
-
 --Create all tables
 --The order of the tables are important because the FK reference table needs to exist before adding a FK.
 CREATE TABLE Assets.Models
@@ -106,7 +98,7 @@ CREATE TABLE Assets.Switches
 	SwitchID	INT	IDENTITY(1,1)	NOT NULL,
 	SwitchName	NVARCHAR(30)		NOT NULL,
 	ModelID		INT					NOT NULL,
-	PortRange	INT					NOT NULL, --FA0/1-12 e.g, change this to include speed(fa, gi)
+	PortRange	NVARCHAR(50)		NOT NULL,
 	CONSTRAINT	PK_SwitchID
 	PRIMARY KEY	(SwitchID),
 	CONSTRAINT	FK_Models_ModelID_Switches
@@ -117,33 +109,12 @@ CREATE TABLE Assets.Switches
 );
 GO
 
---Currently deprecated, not used at the moment.
---CREATE TABLE Assets.Switches_JSON
---(
---	SwitchID	INT	IDENTITY(1,1)	NOT NULL,
---	SwitchName	NVARCHAR(30)		NOT NULL,
---	ModelID		INT					NOT NULL,
---	PortSpeed	INT					NOT NULL,
---	PortRange	INT					NOT NULL,
---	Zones		NVARCHAR(200)		NOT NULL,--JSON
---	VLANS		NVARCHAR(200)		NOT NULL,--JSON
---	CONSTRAINT	PK_SwitchID_JSON
---	PRIMARY KEY	(SwitchID),
---	CONSTRAINT	FK_Models_ModelID_Switches_JSON
---	FOREIGN KEY	(ModelID)
---	REFERENCES	Assets.Models,
---	CONSTRAINT	UQ_SwitchName_JSON
---	UNIQUE		(SwitchName)
---);
---GO
-
 CREATE TABLE Config.Zones
 (
 	ZoneID		INT	IDENTITY(1,1)	NOT NULL,
 	ZoneName	NVARCHAR(20)		NOT NULL,
 	CONSTRAINT	PK_ZoneID
 	PRIMARY KEY	(ZoneID)
-	--This table might be removed if we are converting to a JSON format on the Swtiches table.
 );
 GO
 
@@ -160,45 +131,8 @@ CREATE TABLE Config.VLANs
 	CONSTRAINT	FK_Zones_ZoneID_VLANs
 	FOREIGN KEY	(ZoneID)
 	REFERENCES	Config.Zones
-	--This table might be removed if we are converting to a JSON format on the Swtiches table.
 );
 GO
-
---Currently deprecated, not used at the moment.
---CREATE TABLE Config.ConfigJSON--Might be replaced by textfiles with config code.
---(
---	DocID	INT	IDENTITY	NOT NULL,
---	JSONDoc	NVARCHAR(4000)	NULL,
---	CONSTRAINT	PK_DocID
---	PRIMARY KEY	(DocID),
---	CONSTRAINT	CK_IFJSON_TRUE
---	CHECK		(ISJSON(JSONDoc) = 1) --Check if the inserted data is properly formated JSON.
---	--If we need an index for our documents we can generate one here, check links:
---	--Small tables: https://docs.microsoft.com/en-us/sql/relational-databases/json/store-json-documents-in-sql-tables?view=sql-server-2017#indexes
---	--Large tables: https://docs.microsoft.com/en-us/sql/relational-databases/json/store-json-documents-in-sql-tables?view=sql-server-2017#large-tables--columnstore-format
---	--Frequently changing documents: https://docs.microsoft.com/en-us/sql/relational-databases/json/store-json-documents-in-sql-tables?view=sql-server-2017#frequently-changing-documents--memory-optimized-tables
---);
---GO
-
---Currently deprecated, not used at the moment.
---CREATE TABLE Config.PortConfig
---(
---	SwitchID	INT	IDENTITY(1,1)	NOT NULL,
---	ZoneID		INT					NOT NULL,
---	DocID		INT					NOT NULL,
---	CONSTRAINT	PK_SwitchID_ZoneID
---	PRIMARY KEY	(SwitchID, ZoneID),
---	CONSTRAINT	FK_Switches_SwitchID_PortConfig
---	FOREIGN KEY	(SwitchID)
---	REFERENCES	Assets.Switches,
---	CONSTRAINT	FK_Zone_ZoneiD_PortConfig
---	FOREIGN KEY	(ZoneID)
---	REFERENCES	Config.Zones,
---	CONSTRAINT	FK_ConfigJSON_DocID_PortConfig
---	FOREIGN KEY	(DocID)
---	REFERENCES	Config.ConfigJSON
---);
---GO
 
 CREATE TABLE Config.Rooms
 (
@@ -239,7 +173,7 @@ CREATE TABLE OrderDetails.Courses
 	StartDate			DATETIME			NOT NULL,
 	EndDate				DATETIME			NOT NULL,
 	OrganizerID			INT					NULL,
-	CourseTrainer		NVARCHAR(30)		NULL,
+	CourseTrainer		NVARCHAR(150)		NULL,
 	CONSTRAINT			PK_CourseID
 	PRIMARY KEY			(CourseID),
 	CONSTRAINT			FK_Rooms_RoomID_Courses
@@ -483,7 +417,7 @@ GO
 CREATE PROCEDURE Assets.AddSwitch(
 	@SwitchName NVARCHAR(12) = N'',
 	@ModelID	INT,
-	@Speed		INT
+	@PortRange	NVARCHAR(50)
 )
 AS
 BEGIN TRY
@@ -492,8 +426,8 @@ BEGIN TRY
 	BEGIN TRANSACTION
 		IF NOT EXISTS (SELECT SwitchID FROM Assets.Switches WHERE SwitchName LIKE '%'+@SwitchName+'%')
 			BEGIN
-				INSERT INTO Assets.Switches(SwitchName, ModelID, PortSpeed)
-				VALUES		(@SwitchName, @ModelID, @Speed)
+				INSERT INTO Assets.Switches(SwitchName, ModelID, PortRange)
+				VALUES		(@SwitchName, @ModelID, @PortRange)
 				COMMIT TRANSACTION;
 				RETURN 1;
 			END
@@ -508,39 +442,6 @@ BEGIN CATCH
 	RETURN 0;					--Return False to the application, so we can report the error to the user.
 END CATCH;
 GO
-
---Currently deprecated, not used at the moment.
---CREATE PROCEDURE Assets.AddSwitch_JSON(
---	@SwitchName NVARCHAR(12) = N'',
---	@ModelID	INT,
---	@Speed		INT,
---	@Zones		NVARCHAR(200),
---	@VLANS		NVARCHAR(200)
---)
---AS
---BEGIN TRY
---	SET XACT_ABORT ON;
---	SET NOCOUNT ON;
---	BEGIN TRANSACTION
---		IF NOT EXISTS (SELECT SwitchName FROM Assets.Switches WHERE SwitchName LIKE '%'+@SwitchName+'%')
---			BEGIN
---				INSERT INTO Assets.Switches_JSON(SwitchName, ModelID, PortSpeed, Zones, VLANS)
---				VALUES		(@SwitchName, @ModelID, @Speed, @Zones, @VLANS)
---				COMMIT TRANSACTION;
---				RETURN 1;
---			END
---		ELSE
---			BEGIN
---				ROLLBACK TRANSACTION;
---				THROW 60000, 'SwitchName already exists', 1;
---			END
---END TRY
---BEGIN CATCH
---	EXEC Auditing.ErrorHandling;
---	RETURN 0;					--Return False to the application, so we can report the error to the user.
---END CATCH;
---GO
-
 
 CREATE PROCEDURE Assets.RemoveSwitch(
 	@SwitchID	INT
@@ -573,8 +474,7 @@ CREATE PROCEDURE Assets.UpdateSwitch(
 	@SwitchID	INT = NULL,
 	@SwitchName	NVARCHAR(20),
 	@ModelID	INT,
-	@PortSpeed	INT,
-	@PortRange	INT
+	@PortRange	NVARCHAR(50)
 )
 AS
 BEGIN TRY
@@ -596,7 +496,6 @@ BEGIN TRY
 				UPDATE	Assets.Switches
 				SET		SwitchName = @SwitchName,
 						ModelID = @ModelID,
-						PortSpeed = @PortSpeed,
 						PortRange = @PortRange
 				WHERE	SwitchID = @SwitchID
 				COMMIT TRANSACTION;
@@ -621,7 +520,6 @@ BEGIN TRY
 			SELECT	SW.SwitchID,
 					SW.SwitchName,
 					MO.SwitchModel,
-					SW.PortSpeed,
 					SW.PortRange
 			FROM	Assets.Switches AS SW
 			JOIN	Assets.Models AS MO
@@ -632,44 +530,12 @@ BEGIN TRY
 			SELECT	SW.SwitchID,
 					SW.SwitchName,
 					MO.SwitchModel,
-					SW.PortSpeed,
 					SW.PortRange
 			FROM	Assets.Switches AS SW
 			JOIN	Assets.Models AS MO
 					ON SW.ModelID = MO.ModelID
 			WHERE	SW.SwitchName LIKE '%' + @SwitchName + '%'
 		END
-	SET NOCOUNT OFF;
-END TRY
-BEGIN CATCH
-	EXEC Auditing.ErrorLogging
-	RETURN 0;
-END CATCH;
-GO
-
-
-CREATE PROCEDURE OrderDetails.GetCourses(
-	@CourseID	INT
-)
-AS
-BEGIN TRY
-	SET NOCOUNT ON;
-		SELECT	COR.CourseID,
-				ROO.RoomName,
-				ZON.ZoneName,
-				COR.CourseTitle,
-				COR.CourseDescription,
-				COR.StartDate,
-				COR.EndDate,
-				USR.UserID,
-				COR.CourseTrainer
-		FROM	OrderDetails.Courses AS COR
-		JOIN	Config.Rooms AS ROO
-				ON COR.RoomID = ROO.RoomID
-		JOIN	Config.Zones AS ZON
-				ON COR.ZoneID = ZON.ZoneID
-		JOIN	Person.Users AS USR
-				ON COR.OrganizerID = USR.UserID
 	SET NOCOUNT OFF;
 END TRY
 BEGIN CATCH
@@ -814,7 +680,7 @@ BEGIN TRY
 		ELSE
 			BEGIN
 				ROLLBACK TRANSACTION;
-				THROW 60000, 'ZoneName already exists', 1;
+				THROW 60000, 'VLANID already exists', 1;
 			END
 END TRY
 BEGIN CATCH
@@ -878,8 +744,7 @@ BEGIN TRY
 		ELSE
 			BEGIN
 				UPDATE	Config.VLANs
-				SET		VLANID = @VLANID,
-						SwitchID = @SwitchID,
+				SET		SwitchID = @SwitchID,
 						ZoneID = @ZoneID
 				WHERE	VLANID = @VLANID
 				COMMIT TRANSACTION;
@@ -893,7 +758,7 @@ END CATCH;
 GO
 
 CREATE PROCEDURE Config.GetVLANs(
-	@VLANID	NVARCHAR(20)
+	@VLANID	INT
 )
 AS
 BEGIN TRY
@@ -921,6 +786,335 @@ BEGIN CATCH
 END CATCH;
 GO
 
+CREATE PROCEDURE Config.AddRoom(
+	@SwitchID	INT,
+	@RoomName	NVARCHAR(50),
+	@VLAN		INT
+)
+AS
+BEGIN TRY
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+		IF NOT EXISTS (SELECT RoomID FROM Config.Rooms WHERE RoomName = @RoomName)
+			BEGIN
+				INSERT INTO	Config.Rooms(SwitchID, RoomName, VLAN)
+				VALUES		(@SwitchID, @RoomName, @VLAN)
+				RETURN 1;
+			END
+		ELSE
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 60000, 'RoomName already exists', 1;
+			END
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorHandling;
+	RETURN 0;					--Return False to the application, so we can report the error to the user.
+END CATCH;
+GO
 
+CREATE PROCEDURE Config.RemoveRoom(
+	@RoomID	INT
+)
+AS
+BEGIN TRY
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+		IF (@RoomID IS NULL)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61000, 'RoomID cannot be NULL', 1;
+			END
+		IF NOT EXISTS (SELECT TOP 1 RoomID FROM Config.Rooms WHERE RoomID = @RoomID)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61001, 'RoomID does not exist', 1;
+			END
+		ELSE
+			BEGIN
+				DELETE FROM Config.Rooms
+				WHERE		RoomID = @RoomID
+				COMMIT TRANSACTION;
+				RETURN 1;
+			END
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorHandling;
+	RETURN 0;					--Return False to the application, so we can report the error to the user.
+END CATCH;
+GO
+
+CREATE PROCEDURE Config.UpdateRoom(
+	@RoomID		INT,
+	@SwitchID	INT,
+	@RoomName	NVARCHAR(50),
+	@VLAN		INT
+)
+AS
+BEGIN TRY
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+		IF (@RoomID IS NULL)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61000, 'RoomID cannot be NULL', 1;
+			END
+		IF NOT EXISTS (SELECT TOP 1 RoomID FROM Config.Rooms WHERE RoomID = @RoomID)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61001, 'RoomID does not exist', 1;
+			END
+		ELSE
+			BEGIN
+				UPDATE	Config.Rooms
+				SET		SwitchID = @SwitchID,
+						RoomName = @RoomName,
+						VLAN = @VLAN
+				WHERE	RoomID = @RoomID
+				COMMIT TRANSACTION;
+				RETURN 1;
+			END
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorHandling;
+	RETURN 0;					--Return False to the application, so we can report the error to the user.
+END CATCH;
+GO
+
+CREATE PROCEDURE Config.GetRooms(
+	@RoomID	INT
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	IF @RoomID IS NULL
+		BEGIN
+			SELECT	RoomID,
+					SwitchID,
+					RoomName,
+					VLAN
+			FROM	Config.Rooms
+		END
+	ELSE
+		BEGIN
+			SELECT	RoomID,
+					SwitchID,
+					RoomName,
+					VLAN
+			FROM	Config.Rooms
+			WHERE	RoomID = @RoomID --Might have to change this depending on what we want to filter.
+		END
+	SET NOCOUNT OFF;
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorLogging
+	RETURN 0;
+END CATCH;
+GO
+
+CREATE PROCEDURE Person.AddUser(
+	@FirstName	NVARCHAR(50),
+	@LastName	NVARCHAR(50),
+	@LogonName	NVARCHAR(20),
+	@EMail		NVARCHAR(100)
+)
+AS
+BEGIN TRY
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+		IF NOT EXISTS (SELECT UserID FROM Person.Users WHERE LogonName = @LogonName)
+			BEGIN
+				INSERT INTO	Person.Users(FirstName, LastName, LogonName, EMail)
+				VALUES		(@FirstName, @LastName, @LogonName, @EMail)
+				RETURN 1;
+			END
+		ELSE
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 60000, 'A user with that username already exists', 1;
+			END
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorHandling;
+	RETURN 0;					--Return False to the application, so we can report the error to the user.
+END CATCH;
+GO
+
+CREATE PROCEDURE Person.RemoveUser(
+	@UserID	INT
+)
+AS
+BEGIN TRY
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+		IF (@UserID IS NULL)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61000, 'UserID cannot be NULL', 1;
+			END
+		IF NOT EXISTS (SELECT TOP 1 UserID FROM Person.Users WHERE UserID = @UserID)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61001, 'UserID does not exist', 1;
+			END
+		ELSE
+			BEGIN
+				DELETE FROM Person.Users
+				WHERE		UserID = @UserID
+				COMMIT TRANSACTION;
+				RETURN 1;
+			END
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorHandling;
+	RETURN 0;					--Return False to the application, so we can report the error to the user.
+END CATCH;
+GO
+
+CREATE PROCEDURE Person.UpdateUser(
+	@UserID		INT,
+	@FirstName	NVARCHAR(50),
+	@LastName	NVARCHAR(50),
+	@LogonName	NVARCHAR(20),
+	@EMail		NVARCHAR(100)
+)
+AS
+BEGIN TRY
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+		IF (@UserID IS NULL)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61000, 'UserID cannot be NULL', 1;
+			END
+		IF NOT EXISTS (SELECT TOP 1 UserID FROM Person.Users WHERE UserID = @UserID)
+			BEGIN
+				ROLLBACK TRANSACTION;
+				THROW 61001, 'UserID does not exist', 1;
+			END
+		ELSE
+			BEGIN
+				UPDATE	Person.Users
+				SET		FirstName = @FirstName,
+						LastName = @LastName,
+						LogonName = @LogonName,
+						EMail = @EMail
+				WHERE	UserID = @UserID
+				COMMIT TRANSACTION;
+				RETURN 1;
+			END
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorHandling;
+	RETURN 0;					--Return False to the application, so we can report the error to the user.
+END CATCH;
+GO
+
+CREATE PROCEDURE Person.GetUsers(
+	@UserID	INT
+)
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+	IF @UserID IS NULL
+		BEGIN
+			SELECT	UserID,
+					FirstName,
+					LastName,
+					LogonName,
+					EMail
+			FROM	Person.Users
+		END
+	ELSE
+		BEGIN
+			SELECT	UserID,
+					FirstName,
+					LastName,
+					LogonName,
+					EMail
+			FROM	Person.Users
+			WHERE	UserID = @UserID --Might have to change this depending on what we want to filter.
+		END
+	SET NOCOUNT OFF;
+END TRY
+BEGIN CATCH
+	EXEC Auditing.ErrorLogging
+	RETURN 0;
+END CATCH;
+GO
+
+--CREATE PROCEDURE OrderDetails.AddCourse(
+--	@RoomID				INT,
+--	@ZoneID				INT,
+--	@CourseTitle		NVARCHAR(30),
+--	@CourseDescription	NVARCHAR(250),
+--	@StartDate			DATETIME,
+--	@EndDate			DATETIME,
+--	@OrganizerID		INT,
+--	@CourseTrainer		NVARCHAR(150)
+--)
+--AS
+--BEGIN TRY
+--	SET XACT_ABORT ON;
+--	SET NOCOUNT ON;
+--	BEGIN TRANSACTION
+--		IF NOT EXISTS (SELECT CourseID FROM Person.Users WHERE LogonName = @LogonName)
+--			BEGIN
+--				INSERT INTO	Person.Users()
+--				VALUES		()
+--				RETURN 1;
+--			END
+--		ELSE
+--			BEGIN
+--				ROLLBACK TRANSACTION;
+--				THROW 60000, 'A user with that username already exists', 1;
+--			END
+--END TRY
+--BEGIN CATCH
+--	EXEC Auditing.ErrorHandling;
+--	RETURN 0;					--Return False to the application, so we can report the error to the user.
+--END CATCH;
+--GO
+
+
+
+--CREATE PROCEDURE OrderDetails.GetCourses(
+--	@CourseID	INT
+--)
+--AS
+--BEGIN TRY
+--	SET NOCOUNT ON;
+--		SELECT	COR.CourseID,
+--				ROO.RoomName,
+--				ZON.ZoneName,
+--				COR.CourseTitle,
+--				COR.CourseDescription,
+--				COR.StartDate,
+--				COR.EndDate,
+--				USR.UserID,
+--				COR.CourseTrainer
+--		FROM	OrderDetails.Courses AS COR
+--		JOIN	Config.Rooms AS ROO
+--				ON COR.RoomID = ROO.RoomID
+--		JOIN	Config.Zones AS ZON
+--				ON COR.ZoneID = ZON.ZoneID
+--		JOIN	Person.Users AS USR
+--				ON COR.OrganizerID = USR.UserID
+--	SET NOCOUNT OFF;
+--END TRY
+--BEGIN CATCH
+--	EXEC Auditing.ErrorLogging
+--	RETURN 0;
+--END CATCH;
+--GO
+
+--End the script.
 SET XACT_ABORT OFF;
 GO
+--EOF
