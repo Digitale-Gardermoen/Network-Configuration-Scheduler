ee8cb207a37aa580898c989db701c301ff15ca68
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-09-07 16:03:33 +0200

    Changed DB function
    This function is going to be used to execute procedures, changes are to make it more dynamic.

--- DB.js
+++ DB.js
@@ -5,29 +5,36 @@ const dbConfig = {
     database: 'ZoneController', 
     user: 'test', //Set security to allow SQL logins, it is default to Integrated only.
     password: 'test',
-    
     options: {
         encrypt: false
     }
 }
 
-//Function to connect to database and execute query
-exports.executeQuery = async function(callback, query){
+//Function to connect to database and execute procedures
+exports.executeProcedure = async function(callback, query, inputName, inputVal){
     try {
         const pool = await sql.connect(dbConfig);
-    
         const result = await pool.request()
-            //.input('SwitchName', sql.NVarChar(30), null)
-            //.output('output_parameter', sql.VarChar(50))
+            .input(inputName, inputVal) //InputName = the name of the parameter in the SQL Procedure
+            //------------------------------------
+            //.input(inputName, [sqlDataType], inputVal);
+            //JS Data Type To SQL Data Type Map:
+            //String -> sql.NVarChar
+            //Number -> sql.Int
+            //Boolean -> sql.Bit
+            //Date -> sql.DateTime
+            //Buffer -> sql.VarBinary
+            //sql.Table -> sql.TVP
+            //This should not be needed at all for this application, since we do not have any abstract datatypes in the database. No need for datatype.
+            //------------------------------------
             .execute(query);
-    
-        sql.close();
+        sql.close(); //There is a function to close pools as well, not sure if its better to close the connection in the pool, or the current sql connection
+        //https://www.npmjs.com/package/mssql#close
         callback(result);
     }
     catch(err){
         console.log(err)
     }
-     
     sql.on('error', err => {
         // ... error handler
     })          
--- api/controller/switch.js
+++ api/controller/switch.js
@@ -1,9 +1,11 @@
 const sql = require('../../DB')
 
 exports.GetSwitches = function(req, res) {
-    sql.executeQuery(
+    sql.executeProcedure(
         (result)=>{
             res.end(JSON.stringify(result.recordset))
-        }, 'Assets.GetSwitches'
+        }, 'Assets.GetSwitches',
+        'SwitchName',
+        null //Change this to check if value is given or not.
     )
 }
