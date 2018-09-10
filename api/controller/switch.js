d099d74d087c48334ef9d0300e64c652b8da8b55
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-09-10 21:15:49 +0200

    More routing.
    Added ID params.

--- api/controller/switch.js
+++ api/controller/switch.js
@@ -8,3 +8,12 @@ exports.GetSwitches = function(req, res, inputVal) {
         inputVal //Change this to check if value is given or not.
     )
 }
+
+exports.GetSwitchByID = function(req, res, inputVal) {
+    sql.executeProcedure(
+        (result)=>{res.end(JSON.stringify(result.recordset))},
+        'Assets.GetSwitches',
+        'SwitchID',
+        inputVal //Change this to check if value is given or not.
+    )
+}
--- api/routes/switch.js
+++ api/routes/switch.js
@@ -2,7 +2,17 @@ const switches = require('../controller/switch')
 const router = require('express').Router()
 
 // Main router
-router.route('/:SwitchName')
+router.route('/')
+    .get((req, res) => {
+        switches.GetSwitches(req, res, null)
+    });
+
+router.route('/ID=:SwitchID')
+    .get((req, res) => {
+        switches.GetSwitchByID(req, res, req.params.SwitchID)
+    });
+
+router.route('/SW=:SwitchName')
     .get((req, res) => {
         switches.GetSwitches(req, res, req.params.SwitchName)
     });
